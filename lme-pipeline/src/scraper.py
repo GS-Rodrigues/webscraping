8cfdd168a4b6e22a94748bf6ef8459ac02219363
Guilherme da Silva Rodrigues <guilherme_silva_rodrigues@outlook.com.br>
2026-08-01 22:21:09 -0300

    Atualizado logica de acesso ao banco via scraper, atualização de logica de calculo da data de ontem, adicionado log de registro

--- lme-pipeline/src/scraper.py
+++ lme-pipeline/src/scraper.py
@@ -1,62 +1,84 @@
 import requests
 from Insert import insert_row
-from datetime import datetime, timedelta
+from datetime import timedelta, date
 from Db_connection import get_connection
 import date_treatment
 from bs4 import BeautifulSoup
+import logging
 
-conn = get_connection();
-cursor = conn.cursor();
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
-try:
-    yesterday = (datetime.now() - timedelta(days=1)).date()
-    data_str = yesterday.strftime("%m-%Y")
+def get_last_business_day():
 
-    url = f"https://shockmetais.com.br/lme/{data_str}";
-    response = requests.get(url, timeout=10);
-    response.raise_for_status()
-    
-    html_content = response.content;
-    
-    soup = BeautifulSoup(html_content, 'html.parser');
-    
-    tables = soup.find_all(
-        name='table',
-        attrs= {'class' : 'table table-hover table-sm table-striped shadow'}
-    );
+    today = date.today()
 
-    if not tables:
-        raise RuntimeError("Tabela LME não encontrada na página")
-    else:
-        table = tables[0]
+    while today.weekday() >= 5:
+        today -= timedelta(days=1)
 
-    
-    for row in table.find_all('tr')[1:-1]:
-        colms = row.find_all('td');
+    return today
 
-        if not colms:
-            continue;
+def run_scraper():
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    try:
+        yesterday = get_last_business_day()
+        data_str = yesterday.strftime("%m-%Y")
+
+        url = f"https://shockmetais.com.br/lme/{data_str}"
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        
+        html_content = response.content
+        
+        soup = BeautifulSoup(html_content, 'html.parser')
+        
+        tables = soup.find_all(
+            name='table',
+            attrs= {'class' : 'table table-hover table-sm table-striped shadow'}
+        )
+
+        if not tables:
+            raise RuntimeError("Tabela LME não encontrada na página")
         else:
-            month_year = colms[0].text.strip();
-            if "feriado" in month_year.lower() or "média" in month_year.lower():
-                continue;
-            
-            raw_value = colms[3].get_text(strip=True)
+            table = tables[0]
 
-            try:
-                aluminium = float(raw_value.replace(" ", "").replace("\n", "").replace(",", ""));
-                date_clean = date_treatment.parse_data_br(month_year, yesterday.year);
-                if date_clean.month == yesterday.month:
-                    aluminium = float(aluminium);
-                    insert_row(cursor, date_clean, aluminium);
-            except ValueError:
+        for row in table.find_all('tr')[1:-1]:
+            colms = row.find_all('td')
+
+            if not colms:
                 continue
-    conn.commit();
+            else:
+                month_year = colms[0].text.strip()
+                if "feriado" in month_year.lower() or "média" in month_year.lower():
+                    continue
+                
+                raw_value = colms[3].get_text(strip=True)
 
-except Exception as e:
-    conn.rollback()
-    print("Erro no scraping LME:", e)
+                try:
+                    aluminium = float(raw_value.replace(" ", "").replace("\n", "").replace(",", ""))
+                    date_clean = date_treatment.parse_data_br(month_year, yesterday.year)
+                    if date_clean.month == yesterday.month:
+                        aluminium = float(aluminium)
+                        insert_row(cursor, date_clean, aluminium)
+                        logging.info(f"Inserindo {aluminium} em {date_clean}")
 
-finally:
-    cursor.close()
-    conn.close()
+                except ValueError:
+                    continue
+        conn.commit()
+
+    except Exception as e:
+        conn.rollback()
+        print("Erro no scraping LME:", e)
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
+if __name__ == "__main__":
+    run_scraper()
