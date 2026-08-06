c038b8c889babea86c21c10d2e51414838982699
Guilherme da Silva Rodrigues <guilherme_silva_rodrigues@outlook.com.br>
2026-08-06 20:24:33 -0300

    fix: corrige nome do banco de dados na chave secreta

--- dollar-pipeline/src/Db_connection.py
+++ dollar-pipeline/src/Db_connection.py
@@ -5,7 +5,7 @@ def get_connection():
     return psycopg2.connect(
         host=os.environ["DB_HOST"],
         port=os.environ["DB_PORT"],
-        database=os.environ["DB_NAME_DOLAR"],
+        database=os.environ["DB_NAME"],
         user=os.environ["DB_USER"],
         password=os.environ["DB_PASSWORD"]
     )
