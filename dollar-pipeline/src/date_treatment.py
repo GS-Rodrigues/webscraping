50282a8e36e4bde185f262bc15a8cd63b3035e17
Guilherme da Silva Rodrigues <guilherme_silva_rodrigues@outlook.com.br>
2026-08-06 20:22:02 -0300

    refactor: converte dollar-pipeline em diretório do projeto

---
+++ dollar-pipeline/src/date_treatment.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+def date_treat(text):
+    #text = "2026-01-16 13:04:26.486"
+    dt = datetime.strptime(text, "%Y-%m-%d %H:%M:%S.%f")
+    return(dt)
---
+++ dollar-pipeline/src/scraper.py
@@ -0,0 +1,36 @@
+import requests
+from Insert import *
+from date_treatment import *
+from Db_connection import *
+
+conn = get_connection();
+cursor = conn.cursor();
+
+try:
+    data_str = datetime.today().strftime("%m-%d-%Y")
+
+    url = f"https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%27{data_str}%27&$top=100&$format=json";
+
+    response = requests.get(url, timeout=10);
+    response.raise_for_status()
+    data = response.json();
+
+    if not data["value"]:
+        raise RuntimeError(f"PTAX sem dados para {data_str}")
+
+    else:
+        info = data["value"][0]
+        purchase_value = info["cotacaoCompra"];
+        selling_value = info["cotacaoVenda"];
+        date_time = info["dataHoraCotacao"];
+        date_time = date_treat(date_time);
+        insert_row(cursor, date_time, purchase_value, selling_value);
+        conn.commit()
+
+except Exception as e:
+    conn.rollback()
+    print("Erro:", e)
+
+finally:
+    cursor.close()
+    conn.close()
